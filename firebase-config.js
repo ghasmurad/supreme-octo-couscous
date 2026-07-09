075494b5c97085743b19d93e450c6177d3bbf439
Ghassan Murad <116676942+ghasmurad@users.noreply.github.com>
2026-07-09 14:00:07 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,15 +7,11 @@
   Leave this as null for local demo mode. Local demo mode works in one browser
   and is good for testing, but it does not sync across everyone’s devices.
 */
-window.HHA_FIREBASE_CONFIG = null;
-
-// Example shape:
-// window.HHA_FIREBASE_CONFIG = {
-//   apiKey: "...",
-//   authDomain: "your-project.firebaseapp.com",
-//   databaseURL: "https://your-project-default-rtdb.firebaseio.com",
-//   projectId: "your-project",
-//   storageBucket: "your-project.appspot.com",
-//   messagingSenderId: "...",
-//   appId: "..."
-// };
+window.HHA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCm9OGrVLCH4ofH5gShc2wgPs_vRJBKa88",
+  authDomain: "happy-hour-arcade.firebaseapp.com",
+  projectId: "happy-hour-arcade",
+  storageBucket: "happy-hour-arcade.firebasestorage.app",
+  messagingSenderId: "804367537319",
+  appId: "1:804367537319:web:fe0484d648642828bdf782"
+};
